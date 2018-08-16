1ab0aa53c40d56261497375f1f4d2acc2ad6b846
Wojciech Maj <wojciech.maj@ocado.com>
2018-08-16 14:57:52 +0200

    Implement lodash.once

---
+++ __mocks__/lodashOnceMock.js
@@ -0,0 +1,6 @@
+/**
+ * lodash.once mock is necessary for unit tests to run, as otherwise
+ * it would return the first value only.
+ * @param {Function} fn Function passed to lodash.once
+ */
+module.exports = fn => fn;
