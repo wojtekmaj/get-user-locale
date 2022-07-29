5fc5d677b9df3c491390798e3c8f59b74568d9dc
Wojciech Maj <kontakt@wojtekmaj.pl>
2022-07-29 11:59:35 +0200

    Refactor getUserLocalesInternal function

--- src/index.js
+++ src/index.js
@@ -4,8 +4,8 @@ function resolver(options) {
   return JSON.stringify(options);
 }
 
-function uniq(arr) {
-  return arr.filter((el, index, self) => self.indexOf(el) === index);
+function uniqDefined(arr) {
+  return arr.filter((el, index) => el && arr.indexOf(el) === index);
 }
 
 function normalizeLocales(arr) {
@@ -25,28 +25,20 @@ function getUserLocalesInternal({ useFallbackLocale = true, fallbackLocale = 'en
   if (typeof window !== 'undefined') {
     const { navigator } = window;
 
-    if (navigator.languages) {
-      languageList = languageList.concat(navigator.languages);
-    }
-    if (navigator.language) {
-      languageList.push(navigator.language);
-    }
-    if (navigator.userLanguage) {
-      languageList.push(navigator.userLanguage);
-    }
-    if (navigator.browserLanguage) {
-      languageList.push(navigator.browserLanguage);
-    }
-    if (navigator.systemLanguage) {
-      languageList.push(navigator.systemLanguage);
-    }
+    languageList = languageList.concat(
+      navigator.languages,
+      navigator.language,
+      navigator.userLanguage,
+      navigator.browserLanguage,
+      navigator.systemLanguage,
+    );
   }
 
   if (useFallbackLocale) {
     languageList.push(fallbackLocale);
   }
 
-  return normalizeLocales(uniq(languageList));
+  return normalizeLocales(uniqDefined(languageList));
 }
 
 export const getUserLocales = memoize(getUserLocalesInternal, resolver);
