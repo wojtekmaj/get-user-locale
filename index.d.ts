3625914b50df5ace01e5adbc9ed01ac09e9e30f8
Wojciech Maj <kontakt@wojtekmaj.pl>
2022-09-22 12:01:16 +0200

    Update Prettier, use new --cache option
    Fixup prettier

--- index.d.ts
+++ index.d.ts
@@ -1,8 +1,8 @@
 declare module 'get-user-locale' {
   export type UserLocaleOptions = {
-    fallbackLocale?: string,
-    useFallbackLocale?: boolean,
-  }
+    fallbackLocale?: string;
+    useFallbackLocale?: boolean;
+  };
   export function getUserLocale(options?: UserLocaleOptions): string;
   export function getUserLocales(options?: UserLocaleOptions): string[];
   export default getUserLocale;
